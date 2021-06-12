e21c6b517f2674f2440c72eb88351872ae60b2bd
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-12 13:46:44 -0500

    Added middleware and logged utilities

--- blog/app.js
+++ blog/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 
