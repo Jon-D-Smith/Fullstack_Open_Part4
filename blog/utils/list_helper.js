313e628060851490bbd22fc6da324bb35dbe544f
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-12 14:39:22 -0500

    Added testing for counting likes

--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+    let total
+    if(!blogs.length){
+        total = 0
+    } else {
+        total = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+    }
+     
+    return total
+    }
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
