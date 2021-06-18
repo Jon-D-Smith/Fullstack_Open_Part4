35857db79a3edbf50f90bcf3bc3837021ef24629
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-17 19:16:30 -0500

    Changed post route to async/await

--- blog/controllers/blogs.js
+++ blog/controllers/blogs.js
@@ -8,14 +8,13 @@ blogRouter.get('/', async (request, response) => {
       
   })
   
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
   
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+   const newBlog = await blog.save()
+      
+  return response.status(201).json(newBlog)
+     
   })
 
   module.exports = blogRouter
